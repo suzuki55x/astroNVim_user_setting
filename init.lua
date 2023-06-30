30c2a29f7e7d7ef05faa0a1bb4db5ba0b7ebcd29
T. Suzuki <st09d18@icloud.com>
2023-06-30 14:47:54 +0900

    Fix Github Copilot bindings

--- init.lua
+++ init.lua
@@ -69,6 +69,7 @@ return {
   -- augroups/autocommands and custom filetypes also this just pure lua so
   -- anything that doesn't fit in the normal config locations above can go here
   polish = function()
+
     -- Set up custom filetypes
     -- vim.filetype.add {
     --   extension = {
@@ -81,5 +82,7 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
   end,
 }
